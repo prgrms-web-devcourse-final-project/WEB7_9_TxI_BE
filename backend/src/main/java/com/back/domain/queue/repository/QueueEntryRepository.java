aa6d3f05b3fa792bebbf1281ab9debb0b3369a79
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-05 11:51:50 +0900

    feat: QueueEntryRepository 설정

---
+++ backend/src/main/java/com/back/domain/queue/repository/QueueEntryRepository.java
@@ -0,0 +1,8 @@
+package com.back.domain.queue.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.back.domain.queue.entity.QueueEntry;
+
+public interface QueueEntryRepository extends JpaRepository<QueueEntry, Long> {
+}
