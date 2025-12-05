03995f1a2a98e16b03b5216cd55c9e8edeaa683d
gksdud1109 <vectorh532@gmail.com>
2025-12-05 12:18:07 +0900

    refactor: baseEntity ID 생성전략 sequence로 수정, 필드-컬럼명 수정

--- backend/src/main/java/com/back/global/entity/BaseEntity.java
+++ backend/src/main/java/com/back/global/entity/BaseEntity.java
@@ -6,10 +6,13 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,15 +25,22 @@ import lombok.Setter;
 public abstract class BaseEntity {
 
 	@Id
-	@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+	@SequenceGenerator(
+		name = "global_seq",          // JPA에서 부를 이름
+		sequenceName = "global_seq",  // 실제 PostgreSQL에 생성될 시퀀스 이름
+		allocationSize = 50           // 미리 땡겨올 ID 개수 -> 성능
+	)
 	@Setter(AccessLevel.PROTECTED)
 	private Long id;
 
+	@Column(name = "created_at")
 	@CreatedDate
-	private LocalDateTime createDate;
+	private LocalDateTime createAt;
 
+	@Column(name = "modified_at")
 	@LastModifiedDate
-	private LocalDateTime modifyDate;
+	private LocalDateTime modifiedAt;
 
 	protected BaseEntity(Long id) {
 		this.id = id;
