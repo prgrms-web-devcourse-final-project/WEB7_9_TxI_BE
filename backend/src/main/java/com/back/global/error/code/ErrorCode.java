9056d5bc1ee6901f6b0cf581d7d56941bf7d0b12
woongcheol Na <32201345@dankook.ac.kr>
2025-12-07 18:16:37 +0900

    init

--- backend/src/main/java/com/back/global/error/code/ErrorCode.java
+++ backend/src/main/java/com/back/global/error/code/ErrorCode.java
@@ -10,8 +10,8 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	//사용자 관련
-	NOT_FOUND_USER(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
-	;
+	NOT_FOUND_USER(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다.");
 	private final HttpStatus httpStatus;
 	private final String message;
 }
+
--- backend/src/main/java/com/back/global/response/ApiResponse.java
+++ backend/src/main/java/com/back/global/response/ApiResponse.java
@@ -10,7 +10,7 @@ public record ApiResponse<T>(
 	String message,
 	T data
 ) {
-	public static <T> ApiResponse<T> ok( String message, T data) {
+	public static <T> ApiResponse<T> ok(String message, T data) {
 		return new ApiResponse<>(HttpStatus.OK, message, data);
 	}
 
@@ -18,11 +18,11 @@ public record ApiResponse<T>(
 		return ok("유저 정보를 불러왔습니다.", data);
 	}
 
-	public static <T> ApiResponse<T> created( String message, T data) {
+	public static <T> ApiResponse<T> created(String message, T data) {
 		return new ApiResponse<>(HttpStatus.CREATED, message, data);
 	}
 
-	public static <T> ApiResponse<T> noContent( String message) {
+	public static <T> ApiResponse<T> noContent(String message) {
 		return new ApiResponse<>(HttpStatus.NO_CONTENT, message, null);
 	}
 
