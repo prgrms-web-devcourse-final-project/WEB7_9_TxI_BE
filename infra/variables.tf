cb29c2c3a662f76f3d0cdab986006410077f9de2
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-16 09:27:19 +0900

    infra: terraform 환경 구축
    * infra: terraform 환경 구축
    
    * gitignore
    
    * gitignore
    
    * chore: ignore secrets.tf
    
    * infra: deploy.yml & backend 조금 수정
    
    * fix: profile 변경
    
    * infra: deploy.yml
    
    * infra: deploy.yml

---
+++ infra/variables.tf
@@ -0,0 +1,36 @@
+
+variable "region" {
+  description = "AWS Region"
+  default     = "ap-northeast-2"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "waitfair"
+}
+
+# 리소스 태그
+variable "team_tag" {
+  description = "Team tag value"
+  type        = string
+  default     = "devcos-teamb1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "root_volume_size" {
+  description = "Root volume size in GB"
+  type        = number
+  default     = 20
+}
+
+
+# variable "prefix" {
+#   description = "Prefix for all resources"
+#   default     = "dev"
+# }
