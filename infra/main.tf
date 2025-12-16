ad0bec3a9de2c243cfa4e54d43aa9ccf1d0da9af
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-16 10:40:48 +0900

    infra: 81 port 추가

--- infra/main.tf
+++ infra/main.tf
@@ -172,6 +172,14 @@ resource "aws_security_group" "sg_1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Nginx Proxy Manager Admin"
+    from_port   = 81
+    to_port     = 81
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 0
