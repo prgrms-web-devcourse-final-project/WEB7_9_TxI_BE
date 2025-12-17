bb717e32940d20323892a2fc117029a525203daf
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-17 16:47:08 +0900

    [fix] Websocket 프론트에서 접근시 403 에러 해결
    * fix: securityconfig ws endpoint 추가
    
    * feat: websocket interpretor 설정

--- backend/src/main/java/com/back/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/back/global/config/WebSocketConfig.java
@@ -1,14 +1,24 @@
 package com.back.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.back.global.properties.SiteProperties;
+import com.back.global.websocket.auth.WebSocketAuthInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	private final WebSocketAuthInterceptor webSocketAuthInterceptor;
+	private final SiteProperties siteProperties;
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -18,6 +28,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 		//클라이언트 발행 경로
 		registry.setApplicationDestinationPrefixes("/app");
+
+		// 특정 유저에게 메시지 보낼 때 사용할 prefix
+		registry.setUserDestinationPrefix("/user");
 	}
 
 	@Override
@@ -26,6 +39,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		//Websocket/stomp 연결 endpoint
 		registry.addEndpoint("/ws")
 			.setAllowedOriginPatterns("*")
+			// .setAllowedOrigins(siteProperties.getFrontUrl()) 프론트 배포 후 변경
 			.withSockJS();
 	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(webSocketAuthInterceptor);
+	}
+
 }
