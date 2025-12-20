b0eb23f3a8381dff946c0936aea07d887d6f031b
gksdud1109 <vectorh532@gmail.com>
2025-12-20 19:44:44 +0900

    refactor: websocket동작 info로깅으로 테스트완료, 프로덕션에서는 debug레벨로 설정해서 로그가 남지 않도록 설정

--- backend/src/main/java/com/back/api/seat/event/SeatEventHandler.java
+++ backend/src/main/java/com/back/api/seat/event/SeatEventHandler.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component;
 import com.back.api.seat.dto.response.SeatStatusMessage;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SeatEventHandler {
@@ -15,6 +17,7 @@ public class SeatEventHandler {
 
 	@EventListener
 	public void handleSeatStatus(SeatStatusMessage msg) {
+		log.debug("SEAT_EVENT_RECEIVED eventId={} seatId={} status={}", msg.eventId(), msg.seatId(), msg.status());
 		publisher.publish(msg);
 	}
 }
