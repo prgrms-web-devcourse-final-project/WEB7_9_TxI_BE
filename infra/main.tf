78976d714b07b880b03b3ecf90e6d081d8b0ebfd
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-22 11:43:52 +0900

    feat: s3 설정 변경

--- infra/main.tf
+++ infra/main.tf
@@ -237,29 +237,12 @@ resource "aws_s3_bucket" "s3_bucket_1" {
 resource "aws_s3_bucket_public_access_block" "s3_bucket_1_public" {
   bucket = aws_s3_bucket.s3_bucket_1.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-# 이미지 조회(GET)만 퍼블릭 허용
-resource "aws_s3_bucket_policy" "s3_bucket_1_policy" {
-  bucket = aws_s3_bucket.s3_bucket_1.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowPublicRead"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = "${aws_s3_bucket.s3_bucket_1.arn}/*"
-      }
-    ]
-  })
-}
 
 
 
