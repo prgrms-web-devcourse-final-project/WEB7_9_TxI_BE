68fa5b6a238d65a08ec6b18d1d0778d552d09f58
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-24 14:26:24 +0900

    refactor: 스케줄러 롤백

--- backend/src/main/java/com/back/api/event/scheduler/EventOpenScheduler.java
+++ backend/src/main/java/com/back/api/event/scheduler/EventOpenScheduler.java
@@ -21,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@Profile({"dev", "perf"})
+@Profile({"perf"})
 public class EventOpenScheduler {
 
 	private final EventRepository eventRepository;
