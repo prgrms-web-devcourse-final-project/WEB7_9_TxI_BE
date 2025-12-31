53c5853a38858f483bf50675adecad3c3e5d1a44
woongcheol Na <32201345@dankook.ac.kr>
2025-12-31 14:23:17 +0900

    [db] : flyway 파일 수정

--- backend/src/main/resources/db/migration/V20251231_00__create_payment_table.sql
+++ backend/src/main/resources/db/migration/V20251231_00__create_payment_table.sql
@@ -1,42 +1,43 @@
 /* =========================================================
- * 1. Table: payment
+ * 1. Table: payments
  * ========================================================= */
-CREATE TABLE IF NOT EXISTS payment (
-    payment_id      BIGSERIAL PRIMARY KEY,
-    payment_key     VARCHAR(255) NOT NULL,
-    order_id        VARCHAR(255) NOT NULL,
-    amount          BIGINT       NOT NULL,
-    method          VARCHAR(50)  NOT NULL,
-    status          VARCHAR(20)  NOT NULL,
+CREATE TABLE IF NOT EXISTS payments
+(
+    payment_id  BIGSERIAL PRIMARY KEY,
+    payment_key VARCHAR(255) NOT NULL,
+    order_id    VARCHAR(255) NOT NULL,
+    amount      BIGINT       NOT NULL,
+    method      VARCHAR(50)  NOT NULL,
+    status      VARCHAR(20)  NOT NULL,
 
-    created_at      TIMESTAMP NULL,
-    modified_at     TIMESTAMP NULL
+    created_at  TIMESTAMP    NULL,
+    modified_at TIMESTAMP    NULL
 );
 
 
 /* =========================================================
- * 2. Alter: payment - add created_at (BaseEntity 상속 추가)
+ * 2. Alter: payments - add created_at (BaseEntity 상속 추가)
  * ========================================================= */
-ALTER TABLE payment
+ALTER TABLE payments
     ADD COLUMN IF NOT EXISTS created_at TIMESTAMP NULL;
 
 
 /* =========================================================
- * 3. Alter: payment - add modified_at (BaseEntity 상속 추가)
+ * 3. Alter: payments - add modified_at (BaseEntity 상속 추가)
  * ========================================================= */
-ALTER TABLE payment
+ALTER TABLE payments
     ADD COLUMN IF NOT EXISTS modified_at TIMESTAMP NULL;
 
 
 /* =========================================================
  * 4. Index: payment_key (조회 성능)
  * ========================================================= */
-CREATE INDEX IF NOT EXISTS idx_payment_payment_key
-    ON payment(payment_key);
+CREATE INDEX IF NOT EXISTS idx_payments_payment_key
+    ON payment (payment_key);
 
 
 /* =========================================================
  * 5. Index: order_id (조회 성능)
  * ========================================================= */
-CREATE INDEX IF NOT EXISTS idx_payment_order_id
-    ON payment(order_id);
+CREATE INDEX IF NOT EXISTS idx_payments_order_id
+    ON payments (order_id);
