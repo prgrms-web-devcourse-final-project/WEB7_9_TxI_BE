cd8e8d754016fe9f278293b4a3175f6dc0f8bf36
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2026-01-02 09:44:47 +0900

    feat: verfiy api 관리자 변경 고려 TODO 작성

--- backend/src/main/java/com/back/api/ticket/controller/QrController.java
+++ backend/src/main/java/com/back/api/ticket/controller/QrController.java
@@ -34,6 +34,7 @@ public class QrController implements QrApi {
 		return ApiResponse.ok("QR 토큰 발급 성공", response);
 	}
 
+	//TODO 관리자 전용 API로 변경 고려
 	@Override
 	@GetMapping("/entry/verify")
 	public ApiResponse<QrValidationResponse> validateQrCode(
